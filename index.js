5d0eeb5c103089dd4f42cf6c850e2267ebaba30b
Saikiran <kiranbrettlee418@gmail.com>
2023-03-01 22:35:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3001; 
+
+
+const io = new Server(server, {
+    cors: {
+        origin: "https://chat-app-kiran.netlify.app",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+      });
+
+    socket.on("send_message", (data) => {
+        // console.log(data);
+        socket.to(data.room).emit("receive_message", data);
+    });  
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log("SERVER RUNNING ON PORT: "+PORT);
+})
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Chat App");
+})
+
+
+// https://chat-app-kiran.netlify.app
